3135785837ecae5180cedf492b056218b57d0881
Anja Pješivac <34810295+pjesivac@users.noreply.github.com>
2021-02-17 21:26:54 +0100

    Kreirani linkovi i rute ka komponentama.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { Books } from "./components/Books";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/books" component={Books}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
       <nav>
-        
+        <div class="nav-wrapper">
+          {/* <a href="#" class="brand-logo">
+            Logo
+          </a> */}
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
