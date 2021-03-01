e0850340a490513e2c0e5f7254038806ff46eee7
Anja Pješivac <34810295+pjesivac@users.noreply.github.com>
2021-03-01 11:34:07 +0100

    Dodavanje Material-UI-a. Izmjene u navbar-u.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,31 @@
 import React from "react";
-import SimpleImageSlider from "react-simple-image-slider";
 import { Link } from "react-router-dom";
-
-const images = [
-  { url: "images/1.jpg" },
-  { url: "images/2.jpg" },
-  { url: "images/3.jpg" },
-  { url: "images/4.jpg" },
-  { url: "images/5.jpg" },
-  { url: "images/6.jpg" },
-  { url: "images/7.jpg" },
-];
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "@material-ui/core/Container";
 
 export const Home = () => {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=redux"
+      );
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-8">
-          <SimpleImageSlider width={200} height={100} images={images} />
-        </div>
-        <div className="col-sm-4 card mt-2">
-          <div className="d-grid gap-2 col-6 mx-auto">
-            <span><i>Welcome to <b>Navbar</b></i></span>
-            <Link to="/login" className="btn btn-info mt-2">Login</Link>
-            <Link to="/register" className="btn btn-info mb-2">Register</Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Link to="/login" className="btn btn-info">
+        Login
+      </Link>
+      <Link to="/register" className="btn btn-info">
+        Register
+      </Link>
+    </Container>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,93 @@
 import React from "react";
+import { useState } from "react";
+import Toolbar from "@material-ui/core/Toolbar";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import LinkMaterial from "@material-ui/core/Link";
 import { Link } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  mainbar: {
+    width: "100%",
+    boxShadow: "0 5px 20px -10px #000",
+  },
+  toolbar: {
+    minHeight: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: theme.spacing(5),
+    },
+  },
+  toolbarTitle: {
+    letterSpacing: 1.25,
+    fontWeight: "bold",
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      marginBottom: theme.spacing(0),
+    },
+  },
+  menuButtons: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
+  },
+  item: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: theme.spacing(3),
+    },
+  },
+}));
 
 export const Navbar = () => {
+  const classes = useStyles();
+  const [activeBtn, setActiveBtn] = useState("home");
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <a className="navbar-brand" href="/">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/books">Books</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/about">About</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+    <div className={classes.mainbar}>
+      <Container>
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="left"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            BOOK STORE
+          </Typography>
+          <Box className={classes.menuButtons}>
+            {["home", "books", "about", "contact"].map((item) => (
+              <Link to={item === "home" ? "/" : "/" + item}>
+                <LinkMaterial
+                  component="button"
+                  variant="body2"
+                  onClick={() => setActiveBtn(item)}
+                  color={activeBtn === item ? "textPrimary" : "textSecondary"}
+                  className={classes.item}
+                  key={item}
+                >
+                  {item.toUpperCase()}
+                </LinkMaterial>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
     </div>
   );
 };
