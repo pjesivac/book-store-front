69d14b75cfeb44f207559227d06d37be89878547
jovanarakocevic <jovana.rakocevic@gmail.cogig>
2021-03-06 06:40:09 +0100

    AddBook slanje podataka backendu

--- src/user/AddBook.js
+++ src/user/AddBook.js
@@ -10,7 +10,8 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
-
+import axios from 'axios';
+import { Redirect } from 'react-router-dom'
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(4),
@@ -37,36 +38,90 @@ const useStyles = makeStyles((theme) => ({
 
 export const AddBook = () => {
   const classes = useStyles();
+  const [state, setState] = useState({
+    name: "",
+    overview: "",
+    price: "",
+    author: "",
+    category: "",
+  });
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+  const handleFile = (e) => {
+    let file = e.target.files[0];
+
+    setFile(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tok = JSON.parse(localStorage.getItem('login'));
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': tok,
+    }
+
+    let newBook = new FormData();
+    newBook.append('image', file);
+    newBook.append('name', state.name);
+    newBook.append('overview', state.overview);
+    newBook.append('price', state.price);
+    newBook.append('author', state.author);
+    newBook.append('category', state.category);
+
+    axios.post('http://0b637c001df5.ngrok.io/api/books', newBook, { headers: headers })
+      .then(function (response) {
+        if (response.status === 200) {
+          return <Redirect to='/' />
+        }
+        else
+          console.log("Some error ocurred");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  };
   return (
     <Container component="main" maxWidth="md">
       <CssBaseline />
       <Paper className={classes.paper}>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
           <div className="row d-flex align-items-baseline">
             <p className="col-2">Name:</p>
-            <TextField className="col-10" size="small" variant="outlined" />
+            <TextField id="name" className="col-10" size="small" variant="outlined" onChange={handleChange} required />
           </div>
           <div className="row d-flex mb-3 align-items-start">
             <p className="col-2">Overview:</p>
             <TextField
+              id="overview"
               size="small"
               className="col-10"
               multiline
               rows={4}
               variant="outlined"
+              onChange={handleChange}
+              required
             />
           </div>
           <div className="row d-flex align-items-baseline">
             <p className="col-2">Price:</p>
-            <TextField className="col-10" size="small" variant="outlined" />
+            <TextField id="price" className="col-10" size="small" variant="outlined" onChange={handleChange} required />
           </div>
           <div className="row d-flex align-items-baseline">
             <p className="col-2">Author:</p>
-            <TextField className="col-10" size="small" variant="outlined" />
+            <TextField id="author" className="col-10" size="small" variant="outlined" onChange={handleChange} required />
           </div>
           <div className="row d-flex align-items-baseline">
             <p className="col-2">Category:</p>
-            <TextField className="col-10" size="small" variant="outlined" />
+            <TextField id="category" className="col-10" size="small" variant="outlined" onChange={handleChange} required />
           </div>
           <div className="row d-flex align-items-baseline">
             <p className="col-2">Image:</p>
@@ -74,6 +129,8 @@ export const AddBook = () => {
               type="file"
               className="form-control col-10 input-file"
               id="customFile"
+              onChange={handleFile}
+              required
             />
           </div>
           <Button
@@ -82,6 +139,7 @@ export const AddBook = () => {
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={handleSubmit}
           >
             Add
           </Button>
